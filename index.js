afbc590eb221c942a245c5793b4ed9f9670afbf5
prescottie <scott.hagan@aol.com>
2018-02-15 14:32:57 -0800

    goals are now dynamically set with chrome.storage.sync as an object the key being the period and the value being an array of goals

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-function saveGoal(period, value) {
 
+function setGoals(period, goals) {
+  const ol = document.getElementById(`${period}GoalList`);
+  goals.forEach( goal => {
+    const li = document.createElement("li");
+    li.classList.add('goal-item');
+    li.innerHTML = goals;
+    ol.prepend(li);
+  });
+}
+
+function getGoals(period) {
+  return new Promise(resolve => {
+    chrome.storage.sync.get(period, (goals) => {
+      if(!goals[period]) {
+        resolve(goals = {});
+      } else {
+        console.log('get goals: ', goals);
+        resolve(goals);
+      }
+    });
+  });
+}
+
+async function saveGoal(period, value) {
+
+  let goals = await getGoals(period);
+  console.log('prepush: ', goals);
+  goals[period].push(value);
+  console.log('postpush: ', goals);
+  
+  chrome.storage.sync.set(goals, () => {
+    console.log('goal saved');
+  });
 }
 
 const dailyGoalInput = document.getElementById('dailyGoal');
 
-dailyGoalInput.addEventListener('submit', () => {
-  saveGoal(daily, dailyGoalInput.value);
-})
+dailyGoalInput.addEventListener('change', () => { 
+  const d = 'daily';
+  saveGoal(d , dailyGoalInput.value); 
+});
+
